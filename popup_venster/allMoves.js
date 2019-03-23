d6d8e8fe3281b496a529a69635e4afbdb67dca36
Wiebe <38142564+Huntski@users.noreply.github.com>
2019-03-23 01:09:16 +0100

    Zij Pop Up Venster
    ik vind javascript leuker omdat het een taal is :c

---
+++ popup_venster/allMoves.js
@@ -0,0 +1,39 @@
+/*
+*   author: Wiebe Ranzijn MD1A
+*/
+
+// menu bar navigation
+let move_content = document.querySelector('.move_content'),
+    move_menu = document.querySelector('.move_menu'),
+    move_all = document.querySelector('.move_all');
+
+move_content.addEventListener('click', () => location.href = 'contentMoves.html');
+move_menu.addEventListener('click', () => location.href = 'menuMoves.html');
+move_all.addEventListener('click', () => location.href = 'allMoves.html');
+
+
+// menu icon btn
+let btn_menu = document.querySelector('.btn_menu');
+btn_menu.addEventListener('click', () => {
+    if (btn_menu.classList[1]) {
+        btn_menu.classList.remove('clicked');
+        console.log(btn_menu.classList);
+        menuClose();
+        return;
+    }
+    btn_menu.classList.add('clicked');
+    menuOpen();
+    console.log(btn_menu.classList);
+});
+
+
+// open & close side menu
+let side_menu = document.querySelector('.side_menu');
+function menuOpen() {
+    side_menu.style.left = '0';
+    side_menu.style.width = '100%';
+}
+function menuClose() {
+    side_menu.style.left = '-300px';
+    side_menu.style.width = '300px';
+}
---
+++ popup_venster/contentMoves.js
@@ -0,0 +1,40 @@
+/*
+*   author: Wiebe Ranzijn MD1A
+*/
+
+// menu bar navigation
+let move_content = document.querySelector('.move_content'),
+    move_menu = document.querySelector('.move_menu'),
+    move_all = document.querySelector('.move_all');
+
+move_content.addEventListener('click', () => location.href = 'contentMoves.html');
+move_menu.addEventListener('click', () => location.href = 'menuMoves.html');
+move_all.addEventListener('click', () => location.href = 'allMoves.html');
+
+
+// menu icon btn
+let btn_menu = document.querySelector('.btn_menu');
+btn_menu.addEventListener('click', () => {
+    if (btn_menu.classList[1]) {
+        btn_menu.classList.remove('clicked');
+        console.log(btn_menu.classList);
+        menuClose();
+        return;
+    }
+    btn_menu.classList.add('clicked');
+    menuOpen();
+    console.log(btn_menu.classList);
+});
+
+
+// open & close side menu
+let side_menu = document.querySelector('.side_menu');
+let content = document.querySelector('.content');
+function menuOpen() {
+    side_menu.style.left = '0px';
+    content.style.left = '300px';
+}
+function menuClose() {
+    side_menu.style.left = '-300px';
+    content.style.left = '10px';
+}
